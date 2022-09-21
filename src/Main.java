024d675d46458026a6a0bd10757d5704a6286b83
YuryShageev <schageev.yur@yandex.ru>
2022-09-21 17:15:41 +0300

    2-я часть про переменные

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        short pineApples = 32767;
+        System.out.println("Ананасов " + pineApples + " кг!");
+
+        float sugar = 3;
+        float onePortion = sugar / 4;
+        System.out.println("Одна порция сахара весит " + onePortion + " кг!");
+
+        byte b = 1;
+        short s = 1;
+        int i = 1;
+        i = i + b + s;
+        System.out.println(i);
     }
 }
