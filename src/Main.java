4f1e8671762ed7a15e3904b47894aee010649dd5
YuryShageev <schageev.yur@yandex.ru>
2022-11-11 00:23:03 +0300

    2-я часть про переменные

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,28 @@
 public class Main {
     public static void main(String[] args) {
         int phoneCost = 18000;
-        byte chewingGumCost = 15;
-        short tentCost = 3500;
-        long planeCost = 78940283754L;
-        float medicineCost = 73.10f;
+        byte chewingGumCost = 2;
+        short tentCost = 786;
+        long planeCost = 987678965549L;
+        float medicineCost = 27.12f;
         double manyMedicinesCost = 637.48929;
+        boolean myFriend = false;
         System.out.println(phoneCost);
         System.out.println(chewingGumCost);
         System.out.println(tentCost);
         System.out.println(planeCost);
         System.out.println(medicineCost);
         System.out.println(manyMedicinesCost);
+
+        float a = 27.12f;
+        long b = 987678965549L;
+        byte c = 2;
+        short d = 786;
+        boolean e = false;
+        short f = 569;
+        short g = -159;
+        int h = 27897;
+        byte i = 67;
 
     }
 }
